0616f8a452d1c2995d3699417164644b162dde86
user <user@example.com>
2019-05-11 04:40:32 +0000

    modified plugin `WP phpMyAdmin` version 2.3

--- wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_BSUvNaE4toY0xXQA8PZw6CL/_session_temp.php
+++ wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_BSUvNaE4toY0xXQA8PZw6CL/_session_temp.php
@@ -1,1 +1,1 @@
-<?php $sess_vars = array("time"=>1557423105, "name"=>"wp_pma_aSKdF7IELYvZRP",  "value"=>"wp_pma_X8QbMeuLiJEdqGYvSsTmkFf",  "require_ip"=>false, "ip"=>"68.183.93.63", "strip_slashes"=>false);
+<?php $sess_vars = array("time"=>1557458811, "name"=>"wp_pma_IFBwngXorasA2h",  "value"=>"wp_pma_VOmTLNzsF6IYhKj5A9ykoMJ",  "require_ip"=>false, "ip"=>"103.228.153.232", "strip_slashes"=>false);
